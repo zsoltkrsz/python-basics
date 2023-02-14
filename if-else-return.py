773e34371da87074fc70f94b9b2c07c4620c3a6f
ZsoltK <48351525+zsoltkrsz@users.noreply.github.com>
2023-02-14 13:42:54 +0100

    Create if-else-return.py

---
+++ if-else-return.py
@@ -0,0 +1,12 @@
+def getMax(first, second):
+  if first > second:
+    print("first parameter is bigger than second")
+
+    return first
+  else:
+    print("second parameter is bigger than first")
+
+    return second
+
+
+print(getMax(4, 5))
